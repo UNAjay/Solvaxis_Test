ed735e6989196b0a184a50bb80a384129c6c9034
Gatis Rudins <gatis.rudins@wunderkraut.com>
2015-08-17 14:39:59 +0300

    region-holder section node template

--- drupal/code/themes/custom/solvaxis/templates/ds-1col--node-section.tpl.php
+++ drupal/code/themes/custom/solvaxis/templates/ds-1col--node-section.tpl.php
@@ -7,11 +7,16 @@
 ?>
 <<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col <?php print $classes;?> clearfix">
 
+<div class="region-holder">
+
   <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
 
   <?php print $ds_content; ?>
+
+</div>
+
 </<?php print $ds_content_wrapper ?>>
 
 <?php if (!empty($drupal_render_children)): ?>
