9a780bd7168f3ea2c7b744b3d3afcc87b69c52ed
Gatis Rudins <gatis.rudins@wunderkraut.com>
2015-08-25 10:21:13 +0300

    Enable colorbox popup for Call To Action links.

--- drupal/code/themes/custom/solvaxis/scripts/solvaxis.js
+++ drupal/code/themes/custom/solvaxis/scripts/solvaxis.js
@@ -28,3 +28,34 @@ Drupal.behaviors.jeeves_images = function (context) {
   return;
 };
 
+
+(function ($) {
+  Drupal.behaviors.action_colorbox = {
+    attach: function (context, settings) {
+
+        $('a.call-to-action.download-pdf').each(function() {
+          var href = $(this).attr('href');
+          var urlVars = getUrlVars(href)['width'];
+          if (urlVars == null) {
+            $(this).colorbox({iframe:true, innerWidth:455, innerHeight:450});
+          }
+        });
+
+    },
+  }
+} (jQuery));
+
+
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars(url)
+{
+    var vars = [], hash;
+    var hashes = url.slice(url.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
