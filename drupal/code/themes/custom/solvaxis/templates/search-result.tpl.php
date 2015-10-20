4bd74d97743505acba8cb4a48435136152294cdd
Wunderkraut <sanita.locmele@wunderkraut.com>
2015-10-20 13:50:26 +0300

    Search results

--- drupal/code/themes/custom/solvaxis/templates/search-result.tpl.php
+++ drupal/code/themes/custom/solvaxis/templates/search-result.tpl.php
@@ -66,14 +66,19 @@
 ?>
 <li class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
-  <h3 class="title"<?php print $title_attributes; ?>>
-    <a href="<?php print $url; ?>"><?php print $title; ?></a>
-  </h3>
   
   <?php print render($teaser['field_partner_logo']); ?>
   <?php print render($teaser['field_news_image']); ?>
   
+  <h3 class="title"<?php print $title_attributes; ?>>
+    <?php
+     print format_date($result['node']->created,"custom","d. m. Y");
+    ?> -
+    <a href="<?php print $url; ?>"><?php print $title; ?></a>
+  </h3>
+  
   <?php print render($title_suffix); ?>
+  
   <div class="search-snippet-info">
     <?php if ($snippet): ?>
       <p class="search-snippet"<?php print $content_attributes; ?>><?php print $snippet; ?></p>
